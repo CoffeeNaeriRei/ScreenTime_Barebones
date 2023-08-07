f08db0be156d38440982eb1b35a8c63a33d26dca
yuncoffee <dong072815@gmail.com>
2023-08-07 17:21:38 +0900

    Initial Commit

---
+++ ScreenTime_Barebones/ScreenTime_BarebonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTime_BarebonesApp.swift
+//  ScreenTime_Barebones
+//
+//  Created by Yun Dongbeom on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTime_BarebonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
