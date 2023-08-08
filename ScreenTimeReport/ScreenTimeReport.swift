b520270c74d6558bcbe47833eb70f2639ed4ef3a
kybeen <rei980627@gmail.com>
2023-08-08 23:33:54 +0900

    [Add] DeviceActivityReport Target 추가 (ScreenTimeReport)

---
+++ ScreenTimeReport/ScreenTimeReport.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenTimeReport.swift
+//  ScreenTimeReport
+//
+//  Created by 김영빈 on 2023/08/08.
+//
+
+import DeviceActivity
+import SwiftUI
+
+@main
+struct ScreenTimeReport: DeviceActivityReportExtension {
+    var body: some DeviceActivityReportScene {
+        // Create a report for each DeviceActivityReport.Context that your app supports.
+        TotalActivityReport { totalActivity in
+            TotalActivityView(totalActivity: totalActivity)
+        }
+        // Add more reports here...
+    }
+}
