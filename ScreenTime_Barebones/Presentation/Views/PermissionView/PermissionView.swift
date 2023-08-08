f44761522f2867f3cb3c49a5ccff973382e20c7a
yuncoffee <dong072815@gmail.com>
2023-08-08 10:32:00 +0900

    [Add]: add swiftlint

---
+++ ScreenTime_Barebones/Presentation/Views/PermissionView/PermissionView.swift
@@ -0,0 +1,20 @@
+//
+//  PermissionView.swift
+//  ScreenTime_Barebones
+//
+//  Created by Yun Dongbeom on 2023/08/08.
+//
+
+import SwiftUI
+
+struct PermissionView: View {
+    var body: some View {
+        Text("I'm Permission")
+    }
+}
+
+struct PermissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        PermissionView()
+    }
+}
