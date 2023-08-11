eabbf8d5f0cea07aeecce1f75bbbe161f56f75fa
kybeen <rei980627@gmail.com>
2023-08-11 10:57:46 +0900

    [Comment] Device Activity Report 관련 주석 작성 #10

--- ScreenTimeReport/TotalActivityReport.swift
+++ ScreenTimeReport/TotalActivityReport.swift
@@ -8,20 +8,26 @@
 import DeviceActivity
 import SwiftUI
 
+// MARK: - 각각의 Device Activity Report들에 대응하는 컨텍스트 정의
 extension DeviceActivityReport.Context {
     // If your app initializes a DeviceActivityReport with this context, then the system will use
     // your extension's corresponding DeviceActivityReportScene to render the contents of the
     // report.
+    /// 해당 리포트의 내용 렌더링에 사용할 DeviceActivityReportScene에 대응하는 익스텐션이 필요합니다.  ex) TotalActivityReport
     static let totalActivity = Self("Total Activity")
 }
 
+// MARK: - Device Activity Report의 내용을 어떻게 구성할 지 설정
 struct TotalActivityReport: DeviceActivityReportScene {
     // Define which context your scene will represent.
+    /// 보여줄 리포트에 대한 컨텍스트를 정의해줍니다.
     let context: DeviceActivityReport.Context = .totalActivity
     
     // Define the custom configuration and the resulting view for this report.
+    /// 어떤 데이터를 사용해서 어떤 뷰를 보여줄 지 정의해줍니다. (SwiftUI View)
     let content: (String) -> TotalActivityView
     
+    /// DeviceActivityResults 데이터를 받아서 필터링
     func makeConfiguration(representing data: DeviceActivityResults<DeviceActivityData>) async -> String {
         // Reformat the data into a configuration that can be used to create
         // the report's view.
