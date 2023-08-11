75720ca751cd66db2d4c6073b4c5e3e95d81d690
kybeen <rei980627@gmail.com>
2023-08-11 11:11:16 +0900

    [Add] ScreenTimeActivityReport.swift 파일 추가

---
+++ ScreenTimeReport/ScreenTimeActivityReport.swift
@@ -0,0 +1,31 @@
+//
+//  ScreenTimeActivityReport.swift
+//  ScreenTimeReport
+//
+//  Created by 김영빈 on 2023/08/11.
+//
+
+// MARK: - Device Activity Report 관련 데이터 모델이 정의되어 있는 파일입니다.
+import Foundation
+
+struct ActivityReport {
+    let totalDuration: TimeInterval
+    let apps: [AppDeviceActivity]
+}
+
+struct AppDeviceActivity: Identifiable {
+    var id: String
+    var displayName: String
+    var duration: TimeInterval
+    var numberOfPickups: Int
+}
+
+extension TimeInterval {
+    /// TimeInterval 타입 값을 00:00 형식의 String으로 변환해주는 메서드
+    func toString() -> String {
+        let time = NSInteger(self)
+        let minutes = (time / 60) % 60
+        let hours = (time / 3600)
+        return String(format: "%0.2d:%0.2d", hours,minutes)
+    }
+}
